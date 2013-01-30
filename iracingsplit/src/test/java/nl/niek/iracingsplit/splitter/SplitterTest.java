3ee70b9778d564158b17bbf88c87f5deeccce580
Niek <niekversteege@gmail.com>
2013-01-30 18:33:13 +0100

    Splitter tests filled out.

--- iracingsplit/src/test/java/nl/niek/iracingsplit/splitter/SplitterTest.java
+++ iracingsplit/src/test/java/nl/niek/iracingsplit/splitter/SplitterTest.java
@@ -1,12 +1,16 @@
 package nl.niek.iracingsplit.splitter;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import nl.niek.iracingsplit.driver.Driver;
 import nl.niek.iracingsplit.driver.IDriverBuilder;
+import nl.niek.iracingsplit.splitter.bucket.SplitBucket;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -22,9 +26,9 @@ public class SplitterTest
 	@Before
 	public void setUp() throws Exception
 	{
-		Set<Driver> drivers = getDefaultDriverList();
+		List<Driver> drivers = getDefaultDriverList();
 
-		builder = new MockDriverBuilder(drivers);
+		builder = new MockDriverBuilder(new HashSet<Driver>(drivers));
 
 		splitter = new Splitter(MAX_CARS_ON_TRACK, MIN_DRIVERS,
 				IRATING_TRESHOLD);
@@ -51,25 +55,61 @@ public class SplitterTest
 	@Test
 	public void testSplit()
 	{
-		fail("Not yet implemented");
+		List<SplitBucket> splits = splitter.split(builder);
+		assertEquals(2, splits.size());
 	}
 
 	@Test
 	public void testOneSplit()
 	{
-		fail("Not yet implemented");
+		List<Driver> drivers = getDefaultDriverList();
+		drivers.remove(31);
+		builder = new MockDriverBuilder(new HashSet<Driver>(drivers));
+		List<SplitBucket> splits = splitter.split(builder);
+		assertEquals(2, splits.size());
 	}
 
 	@Test
-	public void testExactMaxNumbers()
+	public void testExactMaxNumbersThreeSplits()
 	{
-		fail("Not yet implemented");
+		List<Driver> drivers = getDefaultDriverList();
+		drivers.addAll(getDefaultDriverList());
+		drivers.addAll(getDefaultDriverList());
+		drivers.remove(1);
+		drivers.remove(2);
+		drivers.remove(3);
+
+		builder = new MockDriverBuilder(new HashSet<Driver>(drivers));
+		List<SplitBucket> splits = splitter.split(builder);
+		assertEquals(3, splits.size());
 	}
 
 	@Test
+	public void testFourSplitsOneOver()
+	{
+		List<Driver> drivers = getDefaultDriverList();
+		drivers.addAll(getDefaultDriverList());
+		drivers.addAll(getDefaultDriverList());
+		drivers.remove(1);
+		drivers.remove(2);
+
+		builder = new MockDriverBuilder(new HashSet<Driver>(drivers));
+		List<SplitBucket> splits = splitter.split(builder);
+		assertEquals(4, splits.size());
+	}
+
+	@Test(expected = IllegalArgumentException.class)
 	public void testSplitNullBuilder()
 	{
-		fail("Not yet implemented");
+		splitter.split(null);
+	}
+	
+	@Test
+	public void testMockBuilderReturnNull()
+	{
+		builder = new MockDriverNullBuilder();
+		List<SplitBucket> splits = splitter.split(builder);
+		assertEquals(0, splits.size());
 	}
 
 	/**
@@ -94,15 +134,32 @@ public class SplitterTest
 		}
 
 	}
-	
+
 	/**
-	 * Get 31 drivers. 
+	 * Testing class only.
+	 * 
+	 * @author Niek
+	 * 
+	 */
+	private class MockDriverNullBuilder implements IDriverBuilder
+	{
+		@Override
+		public Set<Driver> getDrivers()
+		{
+			return null;
+		}
+
+	}
+
+	/**
+	 * Get 31 drivers.
+	 * 
 	 * @return
 	 */
-	private Set<Driver> getDefaultDriverList()
+	private List<Driver> getDefaultDriverList()
 	{
-		Set<Driver> drivers = new HashSet<Driver>();
-		
+		List<Driver> drivers = new ArrayList<Driver>();
+
 		drivers.add(new Driver(2000));
 		drivers.add(new Driver(2200));
 		drivers.add(new Driver(2300));
@@ -116,25 +173,25 @@ public class SplitterTest
 		drivers.add(new Driver(3300));
 		drivers.add(new Driver(3200));
 		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		drivers.add(new Driver(4000));
-		
+		drivers.add(new Driver(4021));
+		drivers.add(new Driver(4120));
+		drivers.add(new Driver(4400));
+		drivers.add(new Driver(4300));
+		drivers.add(new Driver(4100));
+		drivers.add(new Driver(4020));
+		drivers.add(new Driver(4400));
+		drivers.add(new Driver(3900));
+		drivers.add(new Driver(4002));
+		drivers.add(new Driver(4001));
+		drivers.add(new Driver(5200));
+		drivers.add(new Driver(4700));
+		drivers.add(new Driver(4460));
+		drivers.add(new Driver(5500));
+		drivers.add(new Driver(5900));
+		drivers.add(new Driver(6060));
+		drivers.add(new Driver(8800));
+		drivers.add(new Driver(9000));
+
 		return drivers;
 	}
 }
