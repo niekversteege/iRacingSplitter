ea8b465608a60e0b1d32fa92dbb55469ecfc896c
Niek <niekversteege@gmail.com>
2013-02-15 00:21:33 +0100

    Fix pom. Add openCSV lib. Tossed driverfileparser. Added CSVDriverBuilder and test. They need bodies.

---
+++ iracingsplit/src/main/java/nl/niek/iracingsplit/driver/csv/CSVDriverBuilder.java
@@ -0,0 +1,22 @@
+package nl.niek.iracingsplit.driver.csv;
+
+import java.io.File;
+import java.util.List;
+
+import nl.niek.iracingsplit.driver.Driver;
+import nl.niek.iracingsplit.driver.IDriverBuilder;
+
+public class CSVDriverBuilder implements IDriverBuilder
+{
+	public CSVDriverBuilder(File file)
+	{
+		
+	}
+	
+	@Override
+	public List<Driver> getDrivers()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ iracingsplit/src/test/java/nl/niek/iracingsplit/driver/csv/CSVDriverBuilderTest.java
@@ -0,0 +1,39 @@
+package nl.niek.iracingsplit.driver.csv;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CSVDriverBuilderTest
+{
+
+	@Before
+	public void setUp() throws Exception
+	{
+	}
+
+	@Test
+	public void testCSVDriverBuilder()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCSVDriverBuilderNotCSV()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testCSVDriverBuilderTooLarge()
+	{
+		
+	}
+
+	@Test
+	public void testGetDrivers()
+	{
+		fail("Not yet implemented");
+	}
+}
