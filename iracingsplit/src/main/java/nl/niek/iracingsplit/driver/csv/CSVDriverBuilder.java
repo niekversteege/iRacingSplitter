ea076ccd773ae5553864b2d64f249ee307a10dbe
Niek <niekversteege@gmail.com>
2013-02-15 22:22:01 +0100

    CSVDriverBuilder tests made.

--- iracingsplit/src/main/java/nl/niek/iracingsplit/driver/csv/CSVDriverBuilder.java
+++ iracingsplit/src/main/java/nl/niek/iracingsplit/driver/csv/CSVDriverBuilder.java
@@ -6,11 +6,16 @@ import java.util.List;
 import nl.niek.iracingsplit.driver.Driver;
 import nl.niek.iracingsplit.driver.IDriverBuilder;
 
+import org.apache.log4j.Logger;
+
 public class CSVDriverBuilder implements IDriverBuilder
 {
+	private final Logger log = Logger.getLogger(getClass());
+	
 	public CSVDriverBuilder(File file)
 	{
-		
+		log.info("Attempting to build drivers from file:");
+		log.info(file.getAbsolutePath());
 	}
 	
 	@Override
--- iracingsplit/src/test/java/nl/niek/iracingsplit/driver/csv/CSVDriverBuilderTest.java
+++ iracingsplit/src/test/java/nl/niek/iracingsplit/driver/csv/CSVDriverBuilderTest.java
@@ -1,39 +1,47 @@
 package nl.niek.iracingsplit.driver.csv;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.util.List;
+
+import nl.niek.iracingsplit.driver.Driver;
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class CSVDriverBuilderTest
 {
+	private static final String	RESULTFILE	= "src/test/resources/eventresult_7490894.csv";
+	private static final String	TEXTFILE	= "/src/test/resources/notacsv.txt";
+	private static final String	TOOLARGEFILE	= "/src/test/resources/toolargefile";
+	private CSVDriverBuilder	builder;
 
 	@Before
 	public void setUp() throws Exception
 	{
+		builder = new CSVDriverBuilder(new File(RESULTFILE));
 	}
 
 	@Test
 	public void testCSVDriverBuilder()
 	{
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testCSVDriverBuilderNotCSV()
-	{
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testCSVDriverBuilderTooLarge()
-	{
-		
+		List<Driver> drivers = builder.getDrivers();
+
+		assertNotNull(drivers);
+		assertFalse(drivers.isEmpty());
 	}
 
-	@Test
-	public void testGetDrivers()
+	@Test(expected = IllegalArgumentException.class)
+	public void testCSVDriverBuilderNotCSV()
 	{
-		fail("Not yet implemented");
+		builder = new CSVDriverBuilder(new File(TEXTFILE));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testCSVDriverBuilderTooLarge()
+	{
+		builder = new CSVDriverBuilder(new File(TOOLARGEFILE));
 	}
 }
